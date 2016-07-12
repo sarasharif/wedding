740a6ac76e094b0acd290d2e8a446e471c23d8b2
Sara <sara@sharif.com>
2016-07-11 23:52:27 -0500

    all the files

---
+++ public/core.js
@@ -0,0 +1,40 @@
+var sharifRsvp = angular.module('sharifRsvp', []);
+
+function mainController($scope, $http) {
+    $scope.formData = {};
+    // when landing on the page, get all rsvps and show them
+    $http.get('/api/rsvps')
+        .success(function(data) {
+            $scope.rsvps = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    // when submitting the add form, send the text to the node API
+    $scope.createRsvp = function() {
+        $http.post('/api/rsvps', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {}; // clear the form so our user is ready to enter another
+                $scope.rsvps = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    // delete a rsvp after checking it
+    $scope.deleteRsvp = function(id) {
+        $http.delete('/api/rsvps/' + id)
+            .success(function(data) {
+                $scope.rsvps = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+}
---
+++ server.js
@@ -0,0 +1,119 @@
+// set up ========================
+var express  = require('express');
+var app      = express();                               // create our app w/ express
+var router = express.Router();
+var mongoose = require('mongoose');                     // mongoose for mongodb
+var morgan = require('morgan');             // log requests to the console (express4)
+var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+
+// configuration =================
+
+mongoose.connect('mongodb://Sara:SharifWedding@jello.modulusmongo.net:27017/erA6ryna');     // connect to mongoDB database on modulus.io
+
+app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
+
+// define model =================
+    var Rsvp = mongoose.model('Rsvp', {
+        user : String,
+        name : String
+    });
+
+// routes ======================================================================
+
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
+
+
+router.get("/about",function(req,res){
+  res.sendfile('./public/index.html');
+});
+
+router.get("/details",function(req,res){
+  res.sendfile('./public/details.html');
+});
+
+router.get("/wedding-party",function(req,res){
+  res.sendfile('./public/wedding-party.html');
+});
+
+router.get("/rsvp",function(req,res){
+  res.sendfile('./public/rsvp.html');
+});
+
+router.get("/sharif-rsvp-list",function(req,res){
+  res.sendfile('./public/rsvps.html');
+});
+
+    // api ---------------------------------------------------------------------
+    // get all rsvps
+    app.get('/api/rsvps', function(req, res) {
+
+        // use mongoose to get all rsvps in the database
+        Rsvp.find(function(err, rsvps) {
+
+            // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+            if (err)
+                res.send(err)
+
+            res.json(rsvps); // return all todos in JSON format
+        });
+    });
+
+    // create rsvp and send back all todos after creation
+    app.post('/api/rsvps', function(req, res) {
+
+        // create a rsvp, information comes from AJAX request from Angular
+        Rsvp.create({
+            user : 'user'
+            name : req.body.text,
+            done : false
+        }, function(err, rsvp) {
+            if (err)
+                res.send(err);
+
+            // get and return all the rsvps after you create another
+            Rsvp.find(function(err, rsvps) {
+                if (err)
+                    res.send(err)
+                res.json(rsvps);
+            });
+        });
+
+    });
+
+    // delete a rsvp
+    app.delete('/api/rsvps/:todo_id', function(req, res) {
+        Rsvp.remove({
+            _id : req.params.todo_id
+        }, function(err, rsvp) {
+            if (err)
+                res.send(err);
+
+            // get and return all the rsvps after you create another
+            Rsvp.find(function(err, rsvps) {
+                if (err)
+                    res.send(err)
+                res.json(rsvps);
+            });
+        });
+    });
+
+// application -------------------------------------------------------------
+
+app.use("/", router);
+
+// app.get('*', function(req, res) {
+//   res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+// });
+
+// listen (start app with node server.js) ======================================
+app.listen(8080);
+console.log("App listening on port 8080");
